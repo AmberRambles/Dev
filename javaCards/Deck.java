fbcb8bf5f649988495e869026f0aad5dbe2d640e
Amber Shifflett <amber.z.shifflett@outlook.com>
2022-12-12 14:10:03 -0800

    Fixed deck creation issue
    nested for loops were causing logic error, fixed using if logic and counters

--- javaCards/Deck.java
+++ javaCards/Deck.java
@@ -2,6 +2,7 @@ package javaCards;
 
 public class Deck {
     //Properties
+    private boolean DEBUG = false;
     private byte maxCards = 52;
     public Card[] deck = new Card[maxCards];
     private String[] availableSuits = {"Hearts", "Clubs", "Spades", "Diamonds"};
@@ -10,23 +11,45 @@ public class Deck {
     "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"};
     //Constructor
     public Deck() {
+        byte suitCounter = 0;
+        byte visualValueCounter = 0;
         for (byte mainLoop = 0; mainLoop < maxCards; mainLoop++){ 
             //local card variables for constructing cards
             byte creationIndex = mainLoop;
             String creationVisualValue = "";
             String creationSuit = "";
             String creationColor = "";
-            for (int suitLoop = 0; suitLoop < availableSuits.length; suitLoop++){
-                creationSuit = availableSuits[suitLoop];
-                if((suitLoop == 0) || (suitLoop == 3)){ creationColor = availableColors[0]; }
-                else if((suitLoop == 1) || (suitLoop == 2)){ creationColor = availableColors[1]; }
-                for (int visualValueLoop = 0; visualValueLoop < availableVisualValues.length; 
-                visualValueLoop++){
-                    creationVisualValue = availableVisualValues[visualValueLoop];
-                }
+            if(suitCounter < availableSuits.length){ creationSuit = availableSuits[suitCounter]; }
+            else { System.out.println("suitCounter Error"); }
+
+            if((suitCounter == 0) || (suitCounter == 3)){ creationColor = availableColors[0]; }
+            else if((suitCounter == 1) || (suitCounter == 2)){ creationColor = availableColors[1]; }
+
+            if(visualValueCounter < (availableVisualValues.length - 1)){ creationVisualValue = availableVisualValues[visualValueCounter]; }
+            else if(visualValueCounter == (availableVisualValues.length - 1)){
+                creationVisualValue = availableVisualValues[visualValueCounter];
+                suitCounter++;
+            }
+            else{ System.out.println("visualValueCounter Error"); }
+
+            if(DEBUG){
+                System.out.print("mainLoop: ");
+                System.out.println(String.valueOf(mainLoop));
+                System.out.print("creationIndex: ");
+                System.out.println(String.valueOf(creationIndex));
+                System.out.print("creationVisualValue: ");
+                System.out.println(creationVisualValue);
+                System.out.print("creationSuit: ");
+                System.out.println(creationSuit);
+                System.out.print("creationColor: ");
+                System.out.println(creationColor);
             }
             deck[mainLoop] = new Card(creationIndex, creationVisualValue, creationSuit, 
             creationColor);
+
+            
+            visualValueCounter++;
+            if(visualValueCounter == availableVisualValues.length){ visualValueCounter = 0; }
         }
     }
 }
--- javaCards/JavaCards.java
+++ javaCards/JavaCards.java
@@ -2,10 +2,11 @@ package javaCards;
 
 public class JavaCards {
     public static void main(String[] args) {
-        
-        System.out.println("Hello, Amber.");
+        System.out.println("Beginning of JavaCards.");
+
         Deck testDeck = new Deck();
-        for(int i = 0; i < 52; i++){
+
+        for(int i = 0; i < 52; i++){ //this loop demonstrates that the deck is full and accurate
             testDeck.deck[i].printCardInfo();
         }
     }
