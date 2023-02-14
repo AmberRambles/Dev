82cd8b94baabe24b32f6b458120099f8ff45fe05
Amber Shifflett <amber.z.shifflett@outlook.com>
2023-02-13 19:48:15 -0800

    random animal generator update
    Moved the "moving parts" from sql to JS and created functional implimentation. Next will be expanding the animal and photo options. THEN transitioning back to a sql database if our script is getting too weiighty.

--- randomAnimalGenerator/generator.js
+++ randomAnimalGenerator/generator.js
@@ -1,10 +1,16 @@
-
+const animalArray = ["Cat", "Cow", "Dog", "Donkey"];
+const hrefArray = ["./images/manja-vitolic-gKXKBY-C-Dk-unsplash.jpg", "./images/amber-kipp-Mm_D_kbvTUM-unsplash.jpg", "./images/karsten-winegeart-NE0XGVKTmcA-unsplash.jpg", "./images/jacques-bopp-tE2rnQHjdv8-unsplash.jpg"];
 
 function randNum(number) {
-	// Returns a random integer from 1 to number:
-	return Math.floor(Math.random() * number) + 1;
+	// Returns a random integer from 0 to (number - 1)
+	return Math.floor(Math.random() * number);
 }
 
 function generate(){
-	document.getElementById("resultsContainer").style.display = "block";
+	let index = randNum(animalArray.length);
+	document.getElementById("result").innerHTML = animalArray[index];
+	document.getElementById("resultImage").setAttribute("alt", animalArray[index]);
+	document.getElementById("resultImage").setAttribute("src", hrefArray[index]);
+	document.getElementById("resultsContainer").style.display = "flex";
+	document.getElementById("generate").innerHTML = "Again";
 }
