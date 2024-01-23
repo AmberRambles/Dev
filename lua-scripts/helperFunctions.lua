5b0c524599b20ad11f438a6b0f377d7f39ad98bd
Amber Shifflett <ashiff9@wgu.edu>
2024-01-23 12:05:04 -0800

    Adds printSlow() to helper functions

--- lua-scripts/helperFunctions.lua
+++ lua-scripts/helperFunctions.lua
@@ -7,3 +7,14 @@ function randomInit()
 	random()
 end
 
+function printSlow(usrString, seconds)
+	if seconds == nil
+		then seconds = 1
+	end
+	local startTime = os.time()
+	local endTime = startTime + seconds
+	while endTime >= os.time()
+		do startTime = startTime + 1
+	end
+	print(usrString)
+end
